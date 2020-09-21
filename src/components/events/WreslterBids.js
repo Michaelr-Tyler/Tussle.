02bb9261c681987623e7b56c9f60b86d75a4f851
Michael-Tyler <miketylerabc@gmail.com>
2020-09-21 14:15:57 -0500

    Fixed wrestler bids, organizer bids, and wrestlers my events page

--- src/components/events/WreslterBids.js
+++ src/components/events/WreslterBids.js
@@ -1,30 +1,47 @@
-import React from "react"
+import React, { useContext } from "react"
+import { TechnicalContext } from "../TechnicalProvider"
 import "./Events.css"
+import { UserEventContext } from "./UserEventProvider"
 
-export const WrestlerBid = ( {event, technical, userEvent  }) => {
+export const WrestlerBid = ( {  userEvent  }) => {
+    const {deleteUserEvent} = useContext(UserEventContext)
+    const {technicals} = useContext(TechnicalContext)
+
+    const technical = technicals.find(t => t.id === userEvent.event.technicalId)
 
 
-
-if (userEvent.denied !== false) {
+if (userEvent.denied) {
+    return (
+    <section className="event wrestlerMyEvents denied">
+        <h3>Presenting: {userEvent.event.name}</h3>
+        <div>{userEvent.event.date}</div>
+        <div>In {userEvent.event.locationCity}, {userEvent.event.locationStateCode}</div>
+        <div>Looking for: {technical.type}</div>
+        <div>You Bid: {userEvent.bid}</div>
+        <div>DENIED</div>
+        <button
+        onClick={
+            ()=> deleteUserEvent(userEvent.id || {})
+        }>
+            Delete
+        </button>
+    </section>
+    )
+ } else if(userEvent.booked) {
+     return (
+         <>
+         </>
+     )
+ } else if (!userEvent.denied) {
     return (
     <section className="event wrestlerMyEvents">
-        <h3>Presenting: {event.name}</h3>
-        <div>{event.date}</div>
-        <div>In {event.locationCity}, {event.locationStateCode}</div>
+        <h3>Presenting: {userEvent.event.name}</h3>
+        <div>{userEvent.event.date}</div>
+        <div>In {userEvent.event.locationCity}, {userEvent.event.locationStateCode}</div>
         <div>Looking for: {technical.type}</div>
-        <div>Denied</div>
-        <button>delete</button>
+        <div>You Bid: {userEvent.bid}</div>
+        <div>Pending approval</div>
     </section>
     )
- } else if (userEvent.denied !== true) {
-    return (
-    <section className="event wrestler">
-        <h3>Presenting: {event.name}</h3>
-        <div>{event.date}</div>
-        <div>In {event.locationCity}, {event.locationStateCode}</div>
-        <div>Looking for: {technical.type}</div>
-        
-    </section>
-    )
- }
+ } 
 }
--- src/components/events/WrestlerBidList.js
+++ src/components/events/WrestlerBidList.js
@@ -1,16 +1,16 @@
 import React, { useContext, useEffect } from "react"
-import { TechnicalContext } from "../TechnicalProvider"
+import { EventContext } from "./EventProvider"  
 import { UsersContext } from "../users/UsersProvider"
-import { EventContext } from "./EventProvider"
+import { TechnicalContext } from "../TechnicalProvider"
 import { UserEventContext } from "./UserEventProvider"
-import { OrganizerBids } from "./OrganizerBid";
+import { WrestlerBid } from "./WreslterBids"
+import "./Events.css"
 
-
-export const OrganizerBidList = (props) => {
+export const WrestlerBidList = ({history}) => {
     const {events, getEvents} = useContext(EventContext)
     const {technicals, getTechnicals } = useContext(TechnicalContext)
     const { userEvents, getUserEvents } = useContext(UserEventContext)
-    const { currentUser,getCurrentUser, users, getUsers} = useContext(UsersContext)
+    const { currentUser,getCurrentUser} = useContext(UsersContext)
 
     useEffect(() => {
         getEvents()
@@ -19,25 +19,24 @@ export const OrganizerBidList = (props) => {
         .then(getUserEvents)
     },[])
 
-    const currentUserBidEvents = userEvents.filter((ue) => {
-        const currentUserBidEventsRelationships = events.filter(evt => evt.id === ue.eventId && currentUser.id === evt.userId)
+
+    const currentUserEvents = userEvents.filter((ue) => {
+        const currentUserBidEventsRelationships = events.filter(evt => evt.id === ue.eventId && currentUser.id === ue.userId)
         return currentUserBidEventsRelationships.length !== 0
     }) || {}
-
-
-
+    
+    console.log(currentUserEvents)
 
     return (
         <section className="organizerEventsContainer">
-            <h1>Bids</h1>
-            <div className="events wrestlerBids">
+            <h1>My Events</h1>
+            <div className="events wrestlerMyEvent">
                 {
-                    currentUserBidEvents.map(cue => {
+                    currentUserEvents.map(cue => {
                     return (
                     <>
-                    <OrganizerBids key={cue.id} 
-                    userEvent={cue}
-                    />
+                    <WrestlerBid key={cue.id} 
+                    userEvent = {cue} />
                     </>
                     )
                 })
@@ -45,4 +44,5 @@ export const OrganizerBidList = (props) => {
             </div>
         </section>
     )
+
 }
